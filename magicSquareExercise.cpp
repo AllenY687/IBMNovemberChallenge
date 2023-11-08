9e6571520c32f340f4e6d564c118418108d08ba0
AllenY687 <allenyang687@gmail.com>
2023-11-08 12:31:07 -0500

    November 8 Work

--- magicSquareExercise.cpp
+++ magicSquareExercise.cpp
@@ -58,7 +58,7 @@ array<array<array<short, 4>, 4>, 7040> interpret_file(string filename)
 }
 
 // decoding and encoding grids
-long long hash_grid(int (*grid)[4][4]) {
+long long hash_grid(short (grid)[4][4]) {
     long long hash = 0;
     for (int y = 0; y < 4; y++) {
         for (int x = 0; x < 4; x++) {
@@ -67,8 +67,9 @@ long long hash_grid(int (*grid)[4][4]) {
     }
     return hash;
 }
+// {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}} becomes 0b0001001000110100010101100111100010011010101111001101111011110000 = 1311768467463790320 which is 123456789abcde0 in hex
 
-void unhash_grid(int (*grid)[4][4], long long hash)
+void unhash_grid(short (grid)[4][4], long long hash)
 {
     for (int y = 0; y < 4; y++) {
         for (int x = 0; x < 4; x++) {
@@ -78,30 +79,59 @@ void unhash_grid(int (*grid)[4][4], long long hash)
     }
 }
 
+// Function that outputs true or false depending on solvability of square
+// source: https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+bool check_solvable(long long hash) {
+    bool solvable;
+    
+    short row;
+    short inversions;
+
+    for (short i = 0; i < 16; i++) {
+        if (i != 15) {
+            if (hash[i] > hash[i+1]) {
+                inversions++;
+            }
+        }
+        
+        if (hash[i] == 0) {
+            row = i/4;
+        }
+    }
+    +
+    return row%2 != inversions%2;
+}
+
 // Function that processes grid and returns a list of possible moves
-vector<int> get_moves(int (*grid)[4][4])
+vector<short> get_moves(short grid[4][4])
 {
-    vector<int> moves;
+    vector<short> moves;
     int x, y;
     // find the empty space
-    for (int i = 0; i < 16; i++) {
-        if (grid[i / 4][i % 4] == 0) {
+    for (int i = 0; i < 16; i++)
+    {
+        if (grid[i / 4][i % 4] == 0)
+        {
             x = i / 4;
             y = i % 4;
             break;
         }
     }
     // check if the empty space can move up, down, left, or right
-    if (x > 0) {
+    if (x > 0)
+    {
         moves.push_back(0);
     }
-    if (x < 3) {
+    if (x < 3)
+    {
         moves.push_back(1);
     }
-    if (y > 0) {
+    if (y > 0)
+    {
         moves.push_back(2);
     }
-    if (y < 3) {
+    if (y < 3)
+    {
         moves.push_back(3);
     }
     return moves;
@@ -109,31 +139,41 @@ vector<int> get_moves(int (*grid)[4][4])
 
 // meet in the middle algorithm
 set<long long> visited;
-queue<long long> que;
-void bfs(long long st) {
-    que.push(st);
-    while (!que.empty()) {
-        long long cur = que.front();que.pop();
+queue<long long> que1;
+queue<long long> que2;
+void bfs(long long start, long long end) {
+    que1.push(start);
+while (!que1.empty() || !que2.empty()) {
+        int sz = que1.size();
+        for (int cnt = 1; cnt <= sz; cnt++) {
+            long long cur = que1.front();que1.pop();
+            short unhashedGrid[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+            unhash_grid(unhashedGrid, cur);
+            vector<short> moves = get_moves(unhashedGrid);
+            for (auto move : moves) {
+                // generate a new grid that incorporates the move
+                short newGrid[4][4]; // generate (todo)
+                long long hashedNewGrid = hash_grid(newGrid);
+
+                // check if moving into an already visited node
+
+                // check if the node has already been visited by the other bfs algorithm running from the end. This case results in a "meet in the middle"
+                
+                // update distance
+
+                // push into the queue
+                que1.push(hashedNewGrid);
+            }
+        }
+
+        int sz = que2.size();
         
     }
 }
 
-
 vector<int> path;
 
-int main() {
+int main() {;
     array<array<array<short, 4>, 4>, 7040> magic_s = interpret_file("magicSquares.txt");
-    // output magic_s value
-    for (int i = 0; i < 7040; i++) {
-        for (int j = 0; j < 4; j++) {
-            for (int k = 0; k < 3; k++) {
-                cout << magic_s[i][j][k] << " ";
-            }
-            cout << magic_s[i][j][3] << endl;
-        }
-        cout << endl;
-    }
-
-
     return 0;
 }
